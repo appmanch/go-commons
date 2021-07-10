9b99d81ec3e94f30bbc44d539619453b79cdf627
Aditya Kumar <adityak007mr@gmail.com>
2021-07-10 23:46:04 +0530

    doco update, init test cases

--- http/HTTP.md
+++ http/HTTP.md
@@ -2,20 +2,37 @@
 
 The go-commons `http` package provides enterprise grade http routing capabilities.
 
-Usage
-```
-router := turbo.RegisterTurboEngine()
-router.RegisterTurboRoute("GET", "/api/v1/healthCheck", healthCheck)
-router.RegisterTurboRoute("GET,PUT,POST", "/api/v1/getData", getData)
-
-srv := &http.Server{
-    Handler:        router,
-    Addr:           ":8080",
-    ReadTimeout:    20 * time.Second,
-    WriteTimeout:   20 * time.Second,
+###Common Usage
+```go
+func main() {
+	router := turbo.RegisterTurboEngine()
+    router.
+    }RegisterTurboRoute("/api/v1/healthCheck", healthCheck)
+    router.RegisterTurboRoute("/api/v1/getData", getData)
+    
+    srv := &http.Server{
+        Handler:        router,
+        Addr:           ":8080",
+        ReadTimeout:    20 * time.Second,
+        WriteTimeout:   20 * time.Second,
+    }
+    
+    if err := srv.ListenAndServe(); err != nil {
+        log.Fatalln(err)
+    }
 }
-
-if err := srv.ListenAndServe(); err != nil {
-    log.Fatalln(err)
-}
 ```
+
+###Functionalities exposed
+* RegisterTurboRoute - Registers the new route in the HTTP Server for the API
+  * Methods - Function helps in defining the respective supported methods required by the API
+    FYI: the HTTP methods are case in sensitive, so the methods can be added in any form like below
+    ```go
+    func main() {
+        router := turbo.RegisterTurboEngine()
+        router.RegisterTurboRoute("/api/v1/hello", func(w http.ResponseWriter, r *http.Request) {
+            w.Write([]byte("hello from turbo"))
+        } ).StoreTurboMethod("get", "Post")
+    }
+    ```
+* GetRoutes - Returns the List of all Registered Routes in the Server
--- http/turbo_test.go
+++ http/turbo_test.go
@@ -7,36 +7,9 @@ import (
 	"testing"
 )
 
-func TestRegisterRoutes(t *testing.T) {
-	tests := []struct {
-		name string
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-		})
-	}
-}
-
-func TestRoutesHandler(t *testing.T) {
-	tests := []struct {
-		name string
-		want *TurboEngine
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := RegisterTurboEngine(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("RoutesHandler() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestFunction(t *testing.T) {
 	router := RegisterTurboEngine()
+
 	router.RegisterTurboRoute("/api/v1/hello", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("hello from turbo"))
 	} ).StoreTurboMethod("get", "Post")
@@ -49,4 +22,60 @@ func TestFunction(t *testing.T) {
 
 	log.Println(router.GetRoutes())
 
+}
+
+// create mock server route tests
+
+func TestRegisterTurboEngine(t *testing.T) {
+	tests := []struct {
+		name string
+		want *TurboEngine
+	}{
+		// TODO: Add test cases.
+		{
+			name: "TurboEngine",
+			want: RegisterTurboEngine(),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := RegisterTurboEngine(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RegisterTurboEngine() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestTurboEngine_RegisterTurboRoute(t *testing.T) {
+	type fields struct {
+		routes           []*TurboRoute
+		operation        string
+		isRegex          bool
+		isPathUrlEncoded bool
+	}
+	type args struct {
+		path string
+		f    func(w http.ResponseWriter, r *http.Request)
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   *TurboRoute
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			turboEngine := &TurboEngine{
+				routes:           tt.fields.routes,
+				operation:        tt.fields.operation,
+				isRegex:          tt.fields.isRegex,
+				isPathUrlEncoded: tt.fields.isPathUrlEncoded,
+			}
+			if got := turboEngine.RegisterTurboRoute(tt.args.path, tt.args.f); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("RegisterTurboRoute() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
